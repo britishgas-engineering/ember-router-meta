c08db19850adbd1ce7283f319e6a1cbffa9256fc
mikek2252 <mikek2252@btinternet.com>
2017-11-06 11:30:29 +0000

    feat: added configurable default attributes

--- addon/instance-initializers/route-metadata.js
+++ addon/instance-initializers/route-metadata.js
@@ -2,6 +2,8 @@ import dsl from '../utils/dsl-route-extend';
 
 export function initialize (instance) {
   let routeMetaData = instance.lookup('service:route-metadata');
+  let config = instance.container.lookupFactory('config:enviroment');
+  routeMetaData.setAttrs(config.EmberENV.emberRouterMeta.defaultAttrs);
   dsl.init(routeMetaData);
 }
 
