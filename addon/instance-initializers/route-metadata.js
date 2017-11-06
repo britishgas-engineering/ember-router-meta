5f70f93345d852d18ebe35e6f21fdb6fa0ae6481
mikek2252 <mikek2252@btinternet.com>
2017-11-06 11:31:11 +0000

    fix: type

--- addon/instance-initializers/route-metadata.js
+++ addon/instance-initializers/route-metadata.js
@@ -2,7 +2,7 @@ import dsl from '../utils/dsl-route-extend';
 
 export function initialize (instance) {
   let routeMetaData = instance.lookup('service:route-metadata');
-  let config = instance.container.lookupFactory('config:enviroment');
+  let config = instance.container.lookupFactory('config:environment');
   routeMetaData.setAttrs(config.EmberENV.emberRouterMeta.defaultAttrs);
   dsl.init(routeMetaData);
 }
