0b2ef57d2d6d1d4dffae4ac58920d4f1b59f6c0e
mikek2252 <mikek2252@btinternet.com>
2018-07-13 15:01:24 +0100

    Ember 3 and remove old dependences

--- testem.js
+++ testem.js
@@ -1,6 +1,5 @@
-/* eslint-env node */
 module.exports = {
-  test_page: 'tests/index.html?hidepassed&coverage=true',
+  test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
     'Chrome'
@@ -10,13 +9,17 @@ module.exports = {
   ],
   browser_args: {
     Chrome: {
-      mode: 'ci',
-      args: [
-        '--disable-gpu',
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
         '--headless',
-        '--remote-debugging-port=9222',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
         '--window-size=1440,900'
-      ]
-    },
+      ].filter(Boolean)
+    }
   }
 };
